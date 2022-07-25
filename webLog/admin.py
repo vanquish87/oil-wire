66b10c9deb92a267d8c16791ec15f880834b5d00
Jasmeet Singh <jasmeet.singh@inspiraenterprise.com>
2022-07-25 16:33:18 +0530

    DLIS 90 cents done

--- webLog/admin.py
+++ webLog/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from .models import LogFile, LogColumn
+from .models import LogFile, LogColumn, Frame
 
 # Register your models here.
+class LogColumnAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'frame',
+        'logfile'
+        )
+
 admin.site.register(LogFile)
-admin.site.register(LogColumn)
+admin.site.register(LogColumn, LogColumnAdmin)
+admin.site.register(Frame)

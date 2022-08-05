7d5984ed2d043f2d8ab5c17ac536eec059633637
Jasmeet Singh <jasmeet.singh@inspiraenterprise.com>
2022-08-05 21:31:41 +0530

    whitenoise n elasticsearch-workshop n require for production

--- appONGC/settings.py
+++ appONGC/settings.py
@@ -51,6 +51,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # for live servers
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'appONGC.urls'
